abe135e2118c773f4f0bbcf2d4ea7e089e2467ed
terakim <lwkim@terapixel.co.kr>
2023-11-28 05:42:09 +0000

    Commiting the changes for folder named file2

---
+++ file2/malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/830128.cloudwaysapps.com/dyktspzgrz/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/830128.cloudwaysapps.com/dyktspzgrz/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '7e1fec7e852908c1b9d2b6b92dfe824a');
+	include_once('/home/830128.cloudwaysapps.com/dyktspzgrz/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
